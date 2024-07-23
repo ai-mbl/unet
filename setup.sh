4b8d0e86667218950a927117da4555e182aa94c8
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-07-23 18:02:48 -0400

    chore: :bug: fixes to setup script

--- setup.sh
+++ setup.sh
@@ -6,9 +6,13 @@ conda activate 002-unet-exercise
 # Install additional requirements
 if [[ "$CONDA_DEFAULT_ENV" == "002-unet-exercise" ]]; then
     echo "Environment activated successfully for package installs"
-    conda install -c pytorch -c nvicida -c conda-forge --file requirements.txt -y
+    conda install --file requirements.txt -y -c pytorch -c nvidia -c conda-forge
+    python -m ipykernel install --user --name "002-unet-exercise"
 else
     echo "Failed to activate environment for package installs. Dependencies not installed!"
+fi
+gdown -O kaggle_data.zip 1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp
+unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
 conda deactivate
 # Return to base environment
 conda activate base
