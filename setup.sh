cdbbfa41b2d297bc0b0d9646d0b3a081a4f4b3bb
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-08-22 23:06:24 +0000

    chore: :truck: rename env to remove extra 0

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create environment name based on the exercise name
-conda create -n 002-unet-exercise python=3.10 -y
-conda activate 002-unet-exercise
+conda create -n 02-unet-exercise python=3.10 -y
+conda activate 02-unet-exercise
 # Install additional requirements
-if [[ "$CONDA_DEFAULT_ENV" == "002-unet-exercise" ]]; then
+if [[ "$CONDA_DEFAULT_ENV" == "02-unet-exercise" ]]; then
     echo "Environment activated successfully for package installs"
     conda install --file requirements.txt -y -c pytorch -c nvidia -c conda-forge
-    python -m ipykernel install --user --name "002-unet-exercise"
+    python -m ipykernel install --user --name "02-unet-exercise"
 else
     echo "Failed to activate environment for package installs. Dependencies not installed!"
     exit
